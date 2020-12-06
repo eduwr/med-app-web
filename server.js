151a75ab894f9e8832e7a06ca8762a578d3b2e68
eduwr <eduwric@gmail.com>
2020-12-05 21:06:27 -0300

    feat: chat implementation

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-const express = require('express');
+const express = require('express.io');
 const app = express();
+
+app.http().io();
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
 })
 
-app.use(express.static(__dirname + '/public'));
+app.io.route('send', (req) => {
+    app.io.room(req.data.room).broadcast('message', {
+        message: req.data.message,
+        author: req.data.author
+    })
+})
+
+
+app.io.route('ready', (req) => {
+    req.io.join(req.data)
+    app.io.room(req.data).broadcast('announce', {
+        message: 'New client in the ' + req.data + ' room.'
+    })
+})
 
 app.listen(3000, () => {
     console.log('server listening on port 3000')
